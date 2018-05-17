cf52da964c287a22ce400ba451da1f8ac3436366
Pham Trong Tri <trongtri160592@yahoo.com>
2018-05-17 22:36:37 +0700

    Add initialization of project files

--- app/assets/javascripts/cdeConfig.js
+++ app/assets/javascripts/cdeConfig.js
@@ -9,7 +9,7 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['home/jquery'], factory);
+        define(['jquery'], factory);
     } else {
         // Browser globals
         root.CDEConfig = factory(root.jquery);
