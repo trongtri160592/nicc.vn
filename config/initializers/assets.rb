746bd44d0528ef641fef3727d4e39a162f1dee25
Pham Trong Tri <trongtri92nd@gmail.com>
2018-06-13 22:53:20 +0700

    Change font Monserrat for vietnamese

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( ContentPage.js jquery.js jquery-ui.js Common.js)
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
